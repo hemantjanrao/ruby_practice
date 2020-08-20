8d579b1a4f240a6a1fee26c3544ff75426bfe72f
Hemant <hemant.janrao@wirecard.com>
2020-08-20 21:00:12 +0200

    Updated tests

--- games.rb
+++ games.rb
@@ -7,15 +7,20 @@ puts "Hello #{name} !"
 puts "Guess any number between 1 to 100"
 
 3.times do |i|
-  number = rand(100) +1
+  number = rand(100) + 1
   puts "I got the number now you guess"
 
-  puts "is #{number} is correct?"
+  user_guess = gets
 
-  response = gets
-
-  if response == "yes"
+  if user_guess.to_i == number
+    puts "yes our guess matches"
     break
+  else
+    if i.to_i == 2.to_i
+      puts "Max retry reached"
+      puts "My number was #{number}"
+      break
+    end
   end
 
 end
